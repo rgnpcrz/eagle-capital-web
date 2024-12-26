43d395284ebd2b9a8c7302c8f10d5baa9b80ca9d
rgnpcrz <rigonpacharizzi@gmail.com>
2024-12-26 02:51:11 +0100

    get in touch responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      if (scrollTop > -1) {
+      if (scrollTop > 80) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -29,18 +29,25 @@ export default function Navbar() {
   };
   return (
     <>
-      <div className={`w-full fixed duration-200 overflow-hidden ${scrolled ? "bg-primary text-white" : "bg-white text-dark"}`}>
+      {/* <div className={`w-full fixed duration-200 overflow-hidden ${scrolled ? "bg-primary text-white" : "bg-white text-dark"}`}> */}
+      {/* <div className={`w-full h-screen fixed duration-200 overflow-hidden bg-primary text-white  `}></div> */}
+      <div className={`w-full fixed duration-200 overflow-hidden bg-primary text-white  `}>
         <div className="page-container mx-auto px-3">
-          <div className="h-16 flex justify-between items-center">
+          {/* <div className={`${scrolled ? "h-16" : "h-20"} duration-200 flex justify-between items-center`}> */}
+          <div className={`h-16  duration-200 flex justify-between items-center`}>
             <Link to="/">
-              <img src={scrolled ? logoWhite : logo} className="w-44" />
+              {/* <img src={scrolled ? logoWhite : logo} className="w-44" /> */}
+              <img src={logoWhite} className="w-44" />
             </Link>
-            <div className="flex gap-10">
+            <div className=" gap-10 hidden lg:flex ">
               <Link to="/">Homepage</Link>
-              <Link to="/about-us">Real Estate</Link>
               <Link to="/about-us">News</Link>
-              <Link to="/about-us">Credit</Link>
-              <Link to="/about-us">Contact Us</Link>
+              <Link to="/about-us">Real Estate</Link>
+              <Link to="/about-us">Finance</Link>
+              <Link to="/about-us">Technology</Link>
+              <Link to="/about-us">Meet the Team</Link>
+            </div>
+            <div className="flex lg:hidden gap-10">
               <button type="button" className="" onClick={toggleMenu}>
                 Menu - {JSON.stringify(menu)}
               </button>
--- src/pages/Homepage/components/GetInTouch.jsx
+++ src/pages/Homepage/components/GetInTouch.jsx
@@ -6,32 +6,8 @@ export default function GetInTouch() {
     <>
       <div className=" bg-primary text-white">
         <div className="max-w-screen-lg mx-auto px-3 py-20 text-sm">
-          <div className="flex gap-5">
-            <form className=" bg-silver text-dark w-2/4 p-4">
-              <div className="grid grid-cols-2 gap-4 mb-4">
-                <div>
-                  <input type="text" id="firstName" name="firstName" className="border-dark border-solid border-2  px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="First Name" />
-                </div>
-                <div>
-                  <input type="text" id="lastName" name="lastName" className="border-dark border-solid border-2 px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="Last Name" />
-                </div>
-              </div>
-              <div className="mb-4">
-                <input type="email" id="email" name="email" className="border-dark border-solid border-2 px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="E-mail" />
-              </div>
-              <div className="mb-4">
-                <textarea
-                  id="message"
-                  name="message"
-                  className="border-dark border-solid border-2 px-3 py-2 w-full h-32 resize-none focus:outline-none focus:border-gold"
-                  placeholder="Message"
-                ></textarea>
-              </div>
-              <button type="submit" className="bg-dark hover:bg-secondary duration-200 w-full text-white font-bold py-2 px-4 focus:outline-none">
-                Submit
-              </button>
-            </form>
-            <div className=" h-100 flex flex-col justify-between w-2/4">
+          <div className="grid  md:grid-cols-2 gap-5">
+            <div className=" mx-auto max-w-[500px] flex gap-5 flex-1 flex-col justify-between">
               <div className="pr-24">
                 <h1 className="text-4xl pb-5  ">GET IN TOUCH</h1>
                 <h1 className=" text-base">
@@ -52,6 +28,32 @@ export default function GetInTouch() {
                   Phone
                 </span>
               </div>
+            </div>
+            <div className="order-last">
+              <form className="mx-auto max-w-[500px] bg-silver text-dark  p-4">
+                <div className="grid  grid-cols-2 gap-4 mb-4">
+                  <div>
+                    <input type="text" id="firstName" name="firstName" className="border-dark border-solid border-2  px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="First Name" />
+                  </div>
+                  <div>
+                    <input type="text" id="lastName" name="lastName" className="border-dark border-solid border-2 px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="Last Name" />
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <input type="email" id="email" name="email" className="border-dark border-solid border-2 px-3 py-2 w-full focus:outline-none focus:border-gold" placeholder="E-mail" />
+                </div>
+                <div className="mb-4">
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="border-dark border-solid border-2 px-3 py-2 w-full h-32 resize-none focus:outline-none focus:border-gold"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+                <button type="submit" className="bg-dark hover:bg-secondary duration-200 w-full text-white font-bold py-2 px-4 focus:outline-none">
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
         </div>
