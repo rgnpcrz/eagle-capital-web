1cc75f22a024c79efdd387c31fb2dad6c5f7e839
rgnpcrz <rigonpacharizzi@gmail.com>
2024-12-30 02:09:32 +0100

    added team members

---
+++ src/pages/MeetTheTeam/components/TeamCarousel.jsx
@@ -0,0 +1,71 @@
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
+import { useRef } from "react";
+import teamMembers from "../../../data/teamMembers";
+
+export default function TeamCarousel() {
+  const carouselRef = useRef(null);
+
+  const scrollLeftClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -500, behavior: "smooth" });
+    }
+  };
+
+  const scrollRightClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 500, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="">
+      <div className="page-container ">
+        <div className="py-24">
+          <div className="flex justify-between items-center pb-6">
+            <div className="w-40 text-3xl">
+              <p>Our Team</p>
+            </div>
+            <div className="flex gap-5  ">
+              <button onClick={scrollLeftClick} className="border-2 p-0.5 border-black rounded-full">
+                <IconArrowLeft size={24} />
+              </button>
+
+              <button onClick={scrollRightClick} className="border-2 p-0.5 border-black rounded-full">
+                <IconArrowRight size={24} />
+              </button>
+            </div>
+          </div>
+
+          <div className="relative w-full ">
+            {/* Scrollable Container */}
+            <div ref={carouselRef} className="flex overflow-x-scroll -mx-20 scroll-px-20 overflow-y-hidden  snap-x snap-mandatory scroll-smooth gap-24 no-scrollbar cursor-grab">
+              {teamMembers.map((member, index) => (
+                <>
+                  <div className="flex-shrink-0 w-3/5 relative px-20 snap-center  flex  ">
+                    <div className="flex flex-col   py-12 gap-8">
+                      <div className="w-[350px] h-[350px] rounded-xl overflow-hidden bg-slate-50">
+                        <img className="h-full w-full object-cover object-bottom" src={member.image} alt={member.name} />
+                      </div>
+                      <div className="flex-1 flex flex-col gap-8 ">
+                        <div>
+                          <h1 className="text-3xl pb-2">{member.name}</h1>
+                          <p className="text-xl">{member.position}</p>
+                        </div>
+
+                        <div className="space-y-4">
+                          <p className="text-base">{member.bio}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
+            </div>
+            <div class="bg-gradient-to-r from-white absolute -mx-1 h-full top-0 -left-20 bottom-0 w-20"></div>
+            <div class="bg-gradient-to-l from-white absolute -mx-1 h-full top-0 -right-20 bottom-0 w-20"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
